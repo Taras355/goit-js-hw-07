f6a35bb3b0f84fde4caa2cf8de15acdb0236cd90
Taras355 <113373063+Taras355@users.noreply.github.com>
2023-04-18 19:48:23 +0300

    task1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems
+    .map((item) => {
+        const itemMarkup = `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>
+        </li>`;
+        return itemMarkup;
+    })
+    .join("");
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.innerHTML = galleryMarkup;
+
+galleryEl.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    const galleryInstance = basicLightbox.create(`
+    <img class="gallery__image"
+    src="${event.target.dataset.source}" width="800" height="600" 
+    alt="${event.target.alt}">
+    `);
+    // console.log(`Opening "${event.target.alt}" in lightbox`);
+    galleryInstance.show();
+
+    document.addEventListener("keydown", onKeydown);
+    function onKeydown(event) {
+        // console.log("Pressed:", event.code);
+        if (!galleryInstance.visible()) {
+            document.removeEventListener("keydown", onKeydown);
+        }
+        if (event.code === "Escape") {
+            galleryInstance.close();
+            document.removeEventListener("keydown", onKeydown);
+            return;
+        }
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkup = galleryItems
+    .map((item) => {
+        const itemMarkup = `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    alt="${item.description}"
+                />
+            </a>
+        </li>`;
+        return itemMarkup;
+    })
+    .join("");
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.innerHTML = galleryMarkup;
+
+const lightboxInstance = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250
+});
+
+galleryEl.addEventListener("click", (event) => {
+    event.preventDefault();
+});
